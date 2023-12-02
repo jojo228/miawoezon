76f968d69bcc56a31e42c1e7e5c81990773c8947
tatasanvi <statamarc@gmail.com>
2023-12-02 18:54:22 +0000

    marc 02/12/23 Ajout des liens

--- main/urls.py
+++ main/urls.py
@@ -9,7 +9,59 @@ from . import views
 
 urlpatterns = [
     path("", views.home , name="home"),
+    path("index2", views.index2 , name="index2"),
+    path("index3", views.index3 , name="index3"),
+    path("index4", views.index4 , name="index4"),
     path("coming-soon", views.coming_soon , name="coming-soon"),
+    path("about", views.about , name="about"),
+    path("contacts", views.contacts , name="contacts"),
+    path("author-single", views.authorsingle , name="author-single"),
+    path("help", views.help , name="help"),
+    path("pricing-tables", views.pricingtables , name="pricing-tables"),
+    path("booking-single", views.bookingsingle , name="booking-single"),
+    path("dashboard", views.dashboard , name="dashboard"),
+    path("blog2", views.blog2 , name="blog2"),
+    path("blog-single", views.blogsingle , name="blog-single"),
+    path("dashboard-add-listing", views.dashboardaddlisting , name="dashboard-add-listing"),
+    path("error-page", views.errorpage , name="error-page"),
+    path("invoice", views.invoice , name="invoice"),
+    path("blog", views.blog , name="blog"),
+    path("listing-single", views.listingsingle , name="listing-single"),
+    path("listing-single2", views.listingsingle2 , name="listing-single2"),
+    path("listing-single3", views.listingsingle3 , name="listing-single3"),
+    path("listing-single4", views.listingsingle4 , name="listing-single4"),
+    path("listing", views.listing, name="listing"),
+    path("listing2", views.listing2, name="listing2"),
+    path("listing3", views.listing3, name="listing3"),
+    path("listing4", views.listing4, name="listing4"),
+    path("listing5", views.listing5, name="listing5"),
+    path("listing6", views.listing6, name="listing6"),
+    path("room1", views.room1, name="room1"),
+    path("room2", views.room2, name="room2"),
+    path("room3", views.room3, name="room3"),
+    path("dashboard-bookings", views.dashboardbookings , name="dashboard-bookings"),
+    path("dashboard-listing-table", views.dashboardlistingtable , name="dashboard-listing-table"),
+    path("dashboard-messages", views.dashboardmessages , name="dashboard-messages"),
+    path("dashboard-myprofile", views.dashboardmyprofile , name="dashboard-myprofile"),
+    path("dashboard-password", views.dashboardpassword, name="dashboard-password"),
+    path("dashboard-review", views.dashboardreview , name="dashboard-review"),
+    path("dashboard-add-listing", views.dashboardaddlisting , name="dashboard-add-listing"),
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
    
 ]
 
--- main/views.py
+++ main/views.py
@@ -11,3 +11,183 @@ def home(request):
     return render(request, "index.html")
 
 
+def index2(request):
+    
+    return render(request, "index2.html")
+
+
+
+def index3(request):
+    
+    return render(request, "index3.html")
+
+
+def index4(request):
+    
+    return render(request, "index4.html")
+
+
+
+
+def errorpage(request):
+    
+    return render(request, "404.html")
+
+
+def about(request):
+    
+    return render(request, "about.html")
+
+
+def contacts(request):
+    
+    return render(request, "contacts.html")
+
+
+def invoice(request):
+    
+    return render(request, "invoice.html")
+
+
+def authorsingle(request):
+    
+    return render(request, "author-single.html")
+
+
+def help(request):
+    
+    return render(request, "help.html")
+
+
+def pricingtables(request):
+    
+    return render(request, "pricing-tables.html")
+
+
+def bookingsingle(request):
+    
+    return render(request, "booking-single.html")
+
+
+def dashboard(request):
+    
+    return render(request, "dashboard.html")
+
+
+
+def blog2(request):
+    
+    return render(request, "blog2.html")
+
+
+
+def blogsingle(request):
+    
+    return render(request, "blog-single.html")
+
+
+def dashboardaddlisting(request):
+    
+    return render(request, "dashboard-add-listing.html")
+
+
+
+def blog(request):
+    
+    return render(request, "blog.html")
+
+
+def listingsingle(request):
+    
+    return render(request, "listing-single.html")
+
+
+def listingsingle2(request):
+    
+    return render(request, "listing-single2.html")
+
+
+def listingsingle3(request):
+    
+    return render(request, "listing-single3.html")
+
+
+def listingsingle4(request):
+    
+    return render(request, "listing-single4.html")
+
+
+def listing(request):
+    
+    return render(request, "listing.html")
+
+
+def listing2(request):
+    
+    return render(request, "listing2.html")
+
+
+def listing3(request):
+    
+    return render(request, "listing3.html")
+
+
+def listing4(request):
+    
+    return render(request, "listing4.html")
+
+
+def listing5(request):
+    
+    return render(request, "listing5.html")
+
+
+def listing6(request):
+    
+    return render(request, "listing6.html")
+
+
+def room1(request):
+    
+    return render(request, "room/room1.html")
+
+def room2(request):
+    
+    return render(request, "room/room2.html")
+
+
+def room3(request):
+    
+    return render(request, "room/room3.html")
+
+def dashboardaddlisting(request):
+    
+    return render(request, "dashboard-add-listing.html")
+
+def dashboardbookings(request):
+    
+    return render(request, "dashboard-bookings.html")
+
+def dashboardlistingtable(request):
+    
+    return render(request, "dashboard-listing-table.html")
+
+def dashboardmessages(request):
+    
+    return render(request, "dashboard-messages.html")
+
+def dashboardmyprofile(request):
+    
+    return render(request, "dashboard-myprofile.html")
+
+def dashboardpassword(request):
+    
+    return render(request, "dashboard-password.html")
+
+def dashboardreview(request):
+    
+    return render(request, "dashboard-review.html")
+
+
+
+
