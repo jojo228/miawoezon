aaa23ab1cabd8f3917d3d1d2b035e80349b9a927
jojo228 <49480316+jojo228@users.noreply.github.com>
2024-02-22 14:24:48 +0000

    push

--- authentication/adapters.py
+++ authentication/adapters.py
@@ -3,6 +3,6 @@ from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from django.urls import reverse
 
 class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
-    def get_login_redirect_url(self, request):
+    def get_connect_redirect_url(self, request):
         # Redirect to the profile completion page after successful signup
         return reverse('authentication:complete_profile')
