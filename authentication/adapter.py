1f6a7aab054d4aac01d6b356ee1d79ecf2927896
jojo228 <49480316+jojo228@users.noreply.github.com>
2024-02-23 01:01:43 +0000

    testx

--- authentication/adapter.py
+++ authentication/adapter.py
@@ -1,8 +1,9 @@
 # adapters.py
 from allauth.account.adapter import DefaultAccountAdapter
 from django.urls import reverse
+from django.shortcuts import redirect
 
 class MyAccountAdapter(DefaultAccountAdapter):
     def get_login_redirect_url(self, request):
         # Redirect to the profile completion page after successful signup
-        return ('/authentication/complete_profile')
+        return redirect('/authentication/complete_profile')
